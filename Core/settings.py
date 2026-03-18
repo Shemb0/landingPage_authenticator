d5e272c3d08505f50a08618ad96f3326e9334f80
maxi <maxitunn17@gmail.com>
2026-03-19 00:07:27 +0100

    add email timeout

--- Core/settings.py
+++ Core/settings.py
@@ -144,6 +144,7 @@ AUTH_USER_MODEL="user.User"
 
 # ── EMAIL ─────────────────────────────────────────────────────────
 EMAIL_BACKEND   = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_TIMEOUT   = 10
 EMAIL_HOST      = 'smtp.gmail.com'
 EMAIL_PORT      = 587
 EMAIL_USE_TLS   = True
